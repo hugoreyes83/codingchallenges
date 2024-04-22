02b4c6eda643a33151c57045bc81e5f97c921a89
Hugo Reyes <reyes83@gmail.com>
2024-04-21 22:39:01 -0700

    Fixing pylint issues

--- twoSum/test_two_sum.py
+++ twoSum/test_two_sum.py
@@ -1,9 +1,15 @@
+"""
+Test functions to test two_sum.py
+"""
 import pytest
 from two_sum import two_sum
 
 
-@pytest.mark.parametrize("nums,target,expected", 
+@pytest.mark.parametrize("nums,target,expected",
                         (([2, 7, 11, 15], 9, [0,1]),
                         ))
 def test_two_sum(nums, target, expected):
+    """
+    Test function to test two_sum function
+    """
     assert two_sum(nums,target) == expected
